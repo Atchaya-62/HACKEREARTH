86941c40863a8a38ee0746486061c3f24002c1ba
Atchaya-62 <atchayasaran626@gmail.com>
2024-07-28 23:30:15 +0530

    Create Toggle String.c

---
+++ Toggle String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	char a[100];
+	int i=0;
+	scanf("%s",&a);
+	for (i=0;a[i]!='\0';i++){
+		if(a[i]>='a'&& a[i]<='z' ){
+			a[i]=a[i]-32;
+		}
+		else if(a[i]>='A'&& a[i]<='Z'){
+			a[i]=a[i]+32;
+		}
+	}
+	printf("%s",a);
+	return 0;
+	
+	}
